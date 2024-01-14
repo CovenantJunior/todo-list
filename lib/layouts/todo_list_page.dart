05171f02f22b88d2449e14d3bf29d663b3d40b0a
CovenantJunior <covenanttope@gmail.com>
2024-01-14 01:21:22 +0100

    Add 'intl' to display time formats respectively

--- lib/layouts/todo_list_page.dart
+++ lib/layouts/todo_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
@@ -324,7 +325,7 @@ class _TodoListPageState extends State<TodoListPage> {
               Row(
                 children: [
                   const Text("Modified:"),
-                  Text(plan.modified)
+                  Text("${DateFormat('EEE, MMM d yyyy').format(plan.modified)}")
                 ],
               ),
           ],
