87497d505c0201d4098553d1780b143006bfd480
CovenantJunior <covenanttope@gmail.com>
2024-03-13 19:25:36 +0100

    Setup necessary tweaks for iOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/layouts/todo_list_page.dart';
 import 'package:todo_list/layouts/todo_list_preferences.dart';
@@ -5,21 +6,34 @@ import 'package:todo_list/models/todo_list_database.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/services/notification_service.dart';
 
+// [Android-only] This "Headless Task" is run when the Android app is terminated with `enableHeadless: true`
+// Be sure to annotate your callback function to avoid issues in release mode on Flutter >= 3.3.0
+@pragma('vm:entry-point')
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  String taskId = task.taskId;
+  bool isTimeout = task.timeout;
+  if (isTimeout) {
+    // This task has exceeded its allowed running-time.
+    // You must stop what you're doing and immediately .finish(taskId)
+    print("[BackgroundFetch] Headless task timed-out: $taskId");
+    BackgroundFetch.finish(taskId);
+    return;
+  }
+  print('[BackgroundFetch] Headless event received.');
+  // Do your work here...
+  BackgroundFetch.finish(taskId);
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await TodoListDatabase.initialize();
   NotificationService().initNotifications();
-  runApp(
-    MultiProvider(
-      providers: [
-        // TodoList Database Provider
-        ChangeNotifierProvider(
-          create: (context) => TodoListDatabase(),
-        )
-      ],
-      child: const MyApp()
+  runApp(MultiProvider(providers: [
+    // TodoList Database Provider
+    ChangeNotifierProvider(
+      create: (context) => TodoListDatabase(),
     )
-  );
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -31,7 +45,9 @@ class MyApp extends StatelessWidget {
     // Initialize theme preference
     context.read<TodoListDatabase>().themePreference();
     return MaterialApp(
-      theme: context.watch<TodoListDatabase>().isDark == true ? ThemeData.dark() : ThemeData.light(),
+      theme: context.watch<TodoListDatabase>().isDark == true
+          ? ThemeData.dark()
+          : ThemeData.light(),
       debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {
