c27f42bacfe9f091bc94450344546917a582b482
CovenantJunior <covenanttope@gmail.com>
2024-03-17 20:45:24 +0100

    Restore

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,17 @@ import 'package:todo_list/layouts/todo_list_preferences.dart';
 import 'package:todo_list/models/todo_list_database.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/services/notification_service.dart';
-import 'package:workmanager/workmanager.dart';
-
-@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) {
-    print("Native called background task: $task"); //simpleTask will be emitted here.
-    return Future.value(true);
-  });
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await TodoListDatabase.initialize();
-
   NotificationService().initNotifications();
-
-  Workmanager().initialize(
-    callbackDispatcher, // The top level function, aka callbackDispatcher
-    isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-  );
-  Workmanager().registerOneOffTask("task-identifier", "simpleTask");
-  
-  runApp(
-    MultiProvider(
-      providers: [
-        // TodoList Database Provider
-        ChangeNotifierProvider(
-          create: (context) => TodoListDatabase(),
-        )
-      ],
-      child: const MyApp()
+  runApp(MultiProvider(providers: [
+    // TodoList Database Provider
+    ChangeNotifierProvider(
+      create: (context) => TodoListDatabase(),
     )
-  );
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
