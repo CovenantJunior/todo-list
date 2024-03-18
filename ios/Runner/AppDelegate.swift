b4893f3b5ea1ac119bd1e1d39a0f391575f300b9
CovenantJunior <covenanttope@gmail.com>
2024-03-18 09:33:06 +0100

    Background Service

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,10 @@
 import UIKit
 import Flutter
 import flutter_local_notifications
-import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    WorkmanagerPlugin.registerTask(withIdentifier: "task-identifier")
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
@@ -15,17 +13,7 @@ import workmanager
       GeneratedPluginRegistrant.register(with: registry)
     }
 
-    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
-
-    AppDelegate.registerPlugins(with: self) // Register the app's plugins in the context of a normal run
-        
-    WorkmanagerPlugin.setPluginRegistrantCallback { registry in  
-        // The following code will be called upon WorkmanagerPlugin's registration.
-        // Note : all of the app's plugins may not be required in this context ;
-        // instead of using GeneratedPluginRegistrant.register(with: registry),
-        // you may want to register only specific plugins.
-        AppDelegate.registerPlugins(with: registry)
-    }
+    SwiftFlutterBackgroundServicePlugin.taskIdentifier = "reminder"
 
     GeneratedPluginRegistrant.register(with: self)
   
