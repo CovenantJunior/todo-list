a11493815486d1a3ad80213af5950f1d090e2c94
CovenantJunior <covenanttope@gmail.com>
2024-05-14 18:06:03 +0100

    10 minutes

--- lib/layouts/todo_list_page.dart
+++ lib/layouts/todo_list_page.dart
@@ -915,7 +915,7 @@ class _TodoListPageState extends State<TodoListPage> with SingleTickerProviderSt
     List user = context.watch<TodoListDatabase>().user;
 
     if (context.read<TodoListDatabase>().preferences.first.backup == true && context.read<TodoListDatabase>().preferences.first.autoSync == true && user.isNotEmpty) {
-      Timer.periodic(const Duration(minutes: 30), (timer) {
+      Timer.periodic(const Duration(minutes: 10), (timer) {
         setState(() {
           backingUp = true;
         });
